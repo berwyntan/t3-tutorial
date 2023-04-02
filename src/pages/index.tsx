ea62705fc8a718e8ffb2658f162a0baeb231c67d
Berwyn Tan <berwyntan@gmail.com>
2023-04-02 21:00:27 +0800

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,23 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage } from "~/components/Loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+
+  const [ input, setInput ] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    }
+  });
 
   // console.log(user);
 
@@ -32,7 +44,12 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
@@ -72,7 +89,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-            {[...data, ...data]?.map((fullPost) => (
+            {data.map((fullPost) => (
               <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </div>
