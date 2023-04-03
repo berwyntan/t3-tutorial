d68965bef11554554b46655ea029b57797fc98ae
Berwyn Tan <berwyntan@gmail.com>
2023-04-03 10:55:42 +0800

    Ratelimit the endpoint

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,7 +3,11 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -13,11 +17,27 @@ const filterUserForClient = (user: User) => {
   };
 };
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  // prefix: "@upstash/ratelimit",
+});
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
-      orderBy: [{ createdAt: "desc" }]
+      orderBy: [{ createdAt: "desc" }],
     });
 
     const users = (
@@ -27,7 +47,7 @@ export const postsRouter = createTRPCRouter({
       })
     ).map(filterUserForClient);
 
-    console.log(users);
+    // console.log(users);
 
     return posts.map((post) => {
       {
@@ -48,19 +68,26 @@ export const postsRouter = createTRPCRouter({
     });
   }),
 
-  create: privateProcedure.input(z.object({
-    content: z.string().emoji().min(1).max(280),
-  })).mutation(async ({ctx, input}) => {
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
 
-    const authorId = ctx.userId;
+      const { success } = await ratelimit.limit(authorId);
 
-    const post = await ctx.prisma.post.create({
-      data: {
-        authorId,
-        content: input.content,
-      }
-    })
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
-    return post;
-  })
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
